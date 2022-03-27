64f3320873163420e7e56b76c3a30536374af3c8
NNewbieDev <phuonghuy.4444@gmail.com>
2022-03-27 15:45:59 +0700

    end

--- assets/css/book.js
+++ assets/css/book.js
@@ -4,7 +4,14 @@ const buyBtn = document.querySelector('.buy-btn')
 const quantity = document.querySelector('.display')
 const inc =document.querySelector('.up')
 const dec = document.querySelector('.down')
+const bill = document.querySelector('.contain-bill')
+const priceFilms = document.querySelectorAll('.price')
+const priceCore = document.querySelectorAll('.price-core')
+
+var price = [90000, 150000]
+var i = 0
 var seatAll = document.querySelector('.seat-all')
+var dem = 0 
 window.addEventListener('load', function () {
     var i = 1
     for (i; i < 60; i++) {
@@ -19,46 +26,75 @@ window.addEventListener('load', function () {
             ASeat.classList.toggle('check')
         })
     }
-
+    bill.style.display = "none"
     buyBtn.addEventListener('click', function () {
         for (const ASeat of seat) {
             if (ASeat.classList.contains('check')) {
                 ASeat.innerHTML = "X"
-                localStorage.setItem('seat-booked', JSON.stringify(ASeat.innerHTML))
+                dem++
+            }
+        }
+       
+        console.log (dem)
+        if(dem > 0){
+            bill.style.display = "flex"
+            quantity.innerHTML = dem 
+        
+        }else {
+            bill.style.display = "none"
+        }
+        for (const priceFilm of priceFilms) {
+            if (getDay == 0 || getDay == 6) {
+                priceFilm.innerHTML = (price[1] *quantity.innerHTML).toLocaleString('it-IT', { style: 'currency', currency: 'VND' });
+            }
+            else {
+                priceFilm.innerHTML = (price[2] *quantity.innerHTML).toLocaleString('it-IT', { style: 'currency', currency: 'VND' });
             }
         }
     })
-})
-
-// quantity
-var i = 1
-quantity.innerHTML = i
-inc.addEventListener('click', function() {
-    if(i<=0){
-        i = 0
-    }
-    quantity.innerHTML = i++
-
-})
-dec.addEventListener('click', function() {
-    quantity.innerHTML = i--
-    if (i < 0){
-        quantity.innerHTML = 0 
-    }
-})
 
 // price
-const priceFilms = document.querySelectorAll('.price')
-var price = [90000, 150000]
+
 
 var d = new Date()
 var getDay = d.getDay()
-for (const priceFilm of priceFilms) {
+for (const priceC of priceCore) {
     if (getDay == 0 || getDay == 6) {
-        priceFilm.innerHTML = price[1].toLocaleString('it-IT', { style: 'currency', currency: 'VND' });
+        priceC.innerHTML = price[1].toLocaleString('it-IT', { style: 'currency', currency: 'VND' });
     }
     else {
-        priceFilm.innerHTML = price[2].toLocaleString('it-IT', { style: 'currency', currency: 'VND' });
+        priceC.innerHTML = price[2].toLocaleString('it-IT', { style: 'currency', currency: 'VND' });
     }
 
 }
+
+// quantity
+
+inc.addEventListener('click', function() {
+    quantity.innerHTML = ++i
+    for (const priceFilm of priceFilms) {
+        if (getDay == 0 || getDay == 6) {
+            priceFilm.innerHTML = (price[1] *quantity.innerHTML).toLocaleString('it-IT', { style: 'currency', currency: 'VND' });
+        }
+        else {
+            priceFilm.innerHTML = (price[2] *quantity.innerHTML).toLocaleString('it-IT', { style: 'currency', currency: 'VND' });
+        }
+    }
+})
+dec.addEventListener('click', function() {
+    quantity.innerHTML = --i
+    if (i < 0){
+        quantity.innerHTML = 0 
+    }
+    for (const priceFilm of priceFilms) {
+        if (getDay == 0 || getDay == 6) {
+            priceFilm.innerHTML = (price[1] / i).toLocaleString('it-IT', { style: 'currency', currency: 'VND' });
+        }
+        else {
+            priceFilm.innerHTML = (price[2] / i).toLocaleString('it-IT', { style: 'currency', currency: 'VND' });
+        }
+    
+    }
+})
+
+})
