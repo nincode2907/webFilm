68e4b470b9f34ac3deabab7ade030839c04820dd
Nin 92 <vannin2907lc@gmail.com>
2022-08-07 22:17:56 +0700

    test google sign in

---
+++ assets/js/googleAuthur.js
@@ -0,0 +1,45 @@
+// 426280243211-t6efh54lc5cptlqtegl841pn4jh9qcq6.apps.googleusercontent.com
+// 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
+const googleWay = document.querySelector(".google_way")
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
+
+//
+
+var googleUser = {};
+var startApp = function () {
+    gapi.load('auth2', function () {
+        auth2 = gapi.auth2.init({
+            client_id: '426280243211-t6efh54lc5cptlqtegl841pn4jh9qcq6.apps.googleusercontent.com',
+            cookiepolicy: 'single_host_origin',
+        });
+        attachSignin(googleWay);
+    });
+};
+
+function attachSignin(element) {
+    auth2.attachClickHandler(element, {},
+        function (googleUser) {
+            document.getElementById('name').innerText = "Signed in: " +
+                googleUser.getBasicProfile().getName();
+        },
+        // function (error) {
+        //     alert(JSON.stringify(error, undefined, 2));
+        // }
+    );
+}
+
+startApp();
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,29 @@
 const seeMoreBtn = document.querySelector(".body_content_film_btn_seemore");
 const descriptionFade = document.querySelector(".body_content_about_description");
+const menuBtn = document.querySelector(".header_right_menu_tablet")
+const search = document.querySelector(".header_left_input_wrapper")
+const login = document.querySelector(".header_right_login")
+const loginForm = document.querySelector(".login")
+const closeLoginFormBtn = document.querySelector(".login_wrapper_close")
 
 
 window.addEventListener("scroll", () => {
     let value = window.scrollY;
-    if(value >= 880) {
+    if (value >= 880) {
         seeMoreBtn.classList.add("seeMoreScroll");
         descriptionFade.classList.add("description_fade");
     }
 })
+
+menuBtn.onclick = () => {
+    menuBtn.classList.toggle("fade_menu")
+    search.classList.toggle("search_fade")
+}
+
+login.onclick = () => {
+    loginForm.classList.remove("hidden_form")
+}
+
+closeLoginFormBtn.onclick = () => {
+    loginForm.classList.add("hidden_form")
+}
