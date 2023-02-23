3e2b47135115de45d03d8b1c8fda8dc9d6d3841c
nincode2907 <vannin2907lc@gmail.com>
2023-02-23 21:08:12 +0700

    film ani

--- assets/js/film.js
+++ assets/js/film.js
@@ -10,66 +10,76 @@ async function run() {
     const contentFilm = document.querySelector(".body_content_films")
     const seeMore = document.querySelector(".body_content_film_btn_seemore")
     var isNow = true;
+    var cache = '';
+    var isVisitedFilmsInFuture = false;
 
     const filmsNow = []
     const filmsInFuture = []
-// fetch("https://www.galaxycine.vn/api/movie/showAndComming?")
-//     .then(function (response) { return response.json() })
-//     .then(movie => console.log(movie.movieShowing))
 
-
-    //toogle option
+    //navFilms[0] là tab phim đang chiếu
     navFilms[0].addEventListener('click', () => {
         navFilms[0].classList.add('active');
         navFilms[1].classList.remove('active');
-        contentFilm.innerHTML = "";
+
+        //luu lai bang bien cache
+        let tmp = contentFilm.innerHTML;
+        contentFilm.innerHTML = cache;
+        cache = tmp;
         isNow = true;
-        addThreeFilms(filmsNow)
-        addThreeFilms(filmsNow)
     })
 
+    //navFilms[0] là tab phim sắp chiếu
     navFilms[1].addEventListener('click', () => {
         navFilms[1].classList.add('active');
         navFilms[0].classList.remove('active');
-        contentFilm.innerHTML = "";
+
+        //lưu lai bang bien cache
+        let tmp = contentFilm.innerHTML;
+        contentFilm.innerHTML = cache;
+        cache = tmp;
         isNow = false;
-        addThreeFilms(filmsInFuture)
-        addThreeFilms(filmsInFuture)
+
+        //nếu là lần đầu qua phim sắp chiếu thì thêm 6 phim, lần sau lấy lại cache
+        if(!isVisitedFilmsInFuture) {
+            addThreeFilms(filmsInFuture)
+            addThreeFilms(filmsInFuture)
+            isVisitedFilmsInFuture = true;
+        }
     })
 
     //tạo hàm thêm vào 3 phim 
-    function addThreeFilms(arrayFilms) {
+    // function addThreeFilms(arrayFilms) {
 
-    let html = "<div class='row align-items-start mt-1'>";
-    let ranNum;
-    for (let i = 0; i < 3; i++) {
-        do {
-            ranNum = Math.floor(Math.random() * films.length);
-        } while (arrayFilms.includes(ranNum));
-        arrayFilms.push(ranNum)
-        html += `<div class="col-md-4 film col-12 col-sm-12" id="${ranNum}">
-            <span class="col_main">
-                <img src="./assets/img/films/${films[ranNum].img}.jpg" alt="${films[ranNum].name}" class="col_film">
-                <div class="col_sub">
-                    <button type="button" class="btn btn-outline-warning col_sub_btn">Mua vé</button>
-                </div>
-            </span>
-            <h3 class="col_name">${films[ranNum].name}</h3>
-        </div>`;
-        }
-        html += "</div>";
-        let sourceHtml = contentFilm.innerHTML;
-        sourceHtml += html;
-        contentFilm.innerHTML = sourceHtml;
-        const buys = document.querySelectorAll('.col_sub');
-        buys.forEach((buy, index) => {
-            buy.onclick = () => {
-                let getId = buy.parentElement.parentElement.getAttribute("id")
-                window.localStorage.setItem("user", JSON.stringify(getId));
-                window.location = "./book.html"
-            }
-        })
-    }
+    // let html = "<div class='row align-items-start mt-1'>";
+    // let ranNum;
+    // for (let i = 0; i < 3; i++) {
+    //     do {
+    //         ranNum = Math.floor(Math.random() * films.length);
+    //     } while (arrayFilms.includes(ranNum));
+    //     arrayFilms.push(ranNum)
+    //     html += `<div class="col-md-4 film col-12 col-sm-12" id="${ranNum}">
+    //         <span class="col_main">
+    //             <img src="./assets/img/films/${films[ranNum].img}.jpg" alt="${films[ranNum].name}" class="col_film">
+    //             <div class="col_sub">
+    //                 <button type="button" class="btn btn-outline-warning col_sub_btn">Mua vé</button>
+    //             </div>
+    //         </span>
+    //         <h3 class="col_name">${films[ranNum].name}</h3>
+    //     </div>`;
+    //     }
+    //     html += "</div>";
+    //     let sourceHtml = contentFilm.innerHTML;
+    //     sourceHtml += html;
+    //     contentFilm.innerHTML = sourceHtml;
+    //     const buys = document.querySelectorAll('.col_sub');
+    //     buys.forEach((buy, index) => {
+    //         buy.onclick = () => {
+    //             let getId = buy.parentElement.parentElement.getAttribute("id")
+    //             window.localStorage.setItem("user", JSON.stringify(getId));
+    //             window.location = "./book.html"
+    //         }
+    //     })
+    // }
 
     //bắt sự kiện click vào "see more"
     seeMore.addEventListener("click", () => {
@@ -85,10 +95,57 @@ async function run() {
         }
     })
 
-    addThreeFilms(filmsNow);
-    addThreeFilms(filmsNow);
+    function addThreeFilms(arrayFilms) {
+        let ranNum;
 
+        //tạo element div 
+        let newRow = document.createElement('div');
+        newRow.classList.add('row','mt-1','align-items-start');
+        contentFilm.appendChild(newRow);
+        for (let i = 0; i < 3; i++) {
+
+            //thêm từng phim lần lượt
+            setTimeout(() => {
+            //random film and push index into array film
+            do {
+                ranNum = Math.floor(Math.random() * films.length);
+            } while (arrayFilms.includes(ranNum));
+            arrayFilms.push(ranNum);
+
+            let newFilms = document.createElement('div');
+            newFilms.classList.add('col-md-4','film','col-sm-12','col-12');
+            newFilms.setAttribute("id", ranNum);
+
+            let newSpan = document.createElement('span');
+            newSpan.classList.add('col_main');
+            newSpan.innerHTML = `<img src="./assets/img/films/${films[ranNum].img}.jpg" alt="${films[ranNum].name}" class="col_film">
+            <div class="col_sub buy">
+                <button type="button" class="btn btn-outline-warning col_sub_btn">Mua vé</button>
+            </div>`;
+
+            let newH3 = document.createElement('h3');
+            newH3.classList.add('col_name');
+            newH3.innerHTML = films[ranNum].name;
+
+            newFilms.appendChild(newSpan);
+            newFilms.appendChild(newH3);
+            newRow.appendChild(newFilms);
+            }, 500 * i);
+        }
+        setTimeout(() => {
+            let buys = document.querySelectorAll('.buy');
+            buys.forEach((buy, index) => {
+                buy.onclick = () => {
+                    let getId = buy.parentElement.parentElement.getAttribute("id")
+                    window.localStorage.setItem("user", JSON.stringify(getId));
+                    window.location = "./book.html"
+                }
+            })
+        }, 1100);
+        }
+
+    addThreeFilms(filmsNow);
+    addThreeFilms(filmsNow);
 }
 
 run();
-
